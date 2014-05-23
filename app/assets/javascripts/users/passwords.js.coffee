4e13e5be17f3a1cc08b74600cc9352611c2d2887
David Kuo <s50407s@gmail.com>
2014-05-24 00:29:10 +0800

    Prettier reser password

--- app/assets/javascripts/users/passwords.js.coffee
+++ app/assets/javascripts/users/passwords.js.coffee
@@ -21,5 +21,27 @@ $(document).on 'page:change', ->
 						prompt: ''
 					}
 				]
+			},
+			password: {
+				identifier: 'user[password]',
+				rules: [
+					{
+						type  : 'length[8]',
+						prompt: ''
+					}
+				]
+			},
+			password_confirmation: {
+				identifier: 'user[password_confirmation]',
+				rules: [
+					{
+						type  : 'length[8]',
+						prompt: ''
+					},
+					{
+						type  : 'match[user[password]]',
+						prompt: ''
+					}
+				]
 			}
 		}
--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -28,4 +28,57 @@ class Users::PasswordsController < Devise::PasswordsController
 			render :new
 		end
   end
+
+	# GET /resource/password/edit?reset_password_token=abcdef
+	def edit
+		super
+		token = Devise.token_generator.digest(User, :reset_password_token, params[:reset_password_token])
+		user = User.where(reset_password_token: token).first
+		if user
+			resource.username = user.username
+		else
+			redirect_to new_user_password_path
+		end
+	end
+
+	# PUT /resource/password
+  def update
+		token = Devise.token_generator.digest(User, :reset_password_token, resource_params[:reset_password_token])
+		user = User.where(reset_password_token: token).first
+		if user
+			if resource_params[:password] != resource_params[:password_confirmation]
+				self.resource = User.new do |u|
+					u.reset_password_token = resource_params[:reset_password_token]
+				end
+				@error = "兩次密碼不相同，請重新輸入。"
+				@mark_error = true
+				render :edit
+			elsif !(Devise.password_length.include? resource_params[:password].length)
+				self.resource = User.new do |u|
+					u.reset_password_token = resource_params[:reset_password_token]
+				end
+				@error = "密碼長度必須在 %d ~ %d 之間，請重新輸入。" % [Devise.password_length.min, Devise.password_length.max]
+				@mark_error = true
+				render :edit
+			else
+				self.resource = resource_class.reset_password_by_token(resource_params)
+				yield resource if block_given?
+
+				if resource.errors.empty?
+					resource.unlock_access! if unlockable?(resource)
+					sign_in(resource_name, resource)
+					redirect_to after_resetting_password_path_for(resource)
+				else
+					respond_with resource
+				end
+			end
+		else
+			self.resource = User.new do |u|
+				u.reset_password_token = resource_params[:reset_password_token]
+			end
+			@error = "重設密碼請求已過期或無效，請重新申請。"
+			render :edit
+		end
+  end
+
 end
